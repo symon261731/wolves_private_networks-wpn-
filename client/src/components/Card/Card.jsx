12245da4adb3850146e98c5002fecc77cf99e1e9
Dmitr99 <grishko_dmitriy99@mail.ru>
2022-11-10 16:32:23 +0300

    card fic

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 
 export default function Card() {
   return (
-    <div style={{ border: '2px solid', borderRadius: '10px' }}>
-      <p>User_owner:</p>
-      <p>Protocol:</p>
-      <p>Location:</p>
-      <p>Price:</p>
-      <p>Rating:</p>
+
+    <div className="card" style={{ width: '18rem' }}>
+      <div className="card-body">
+        <p>User_owner:</p>
+        <p>Protocol:</p>
+        <p>Location:</p>
+        <p>Price:</p>
+        <p>Rating:</p>
+      </div>
     </div>
   );
 }
