ba6b78feecd9ebe917033010cdc901654ee3e94c
Simon Mishchenko <symon261731@gmail.com>
2022-11-10 20:10:43 +0300

    add style in Orders

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,29 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './NavBar.scss';
 
 export default function NavBar() {
   const user = { login: 'admin', id: 1 };
 
   return (
     <div>
-      <ul className="nav justify-content-center">
+      <ul className="nav">
         <li className="nav-item">
           <Link className="nav-link active" aria-current="page" to="/">Home</Link>
         </li>
         {user?.id ? (
           <>
-            <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/personalPage">MyPage</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/orders">Orders</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/addCash">Pocket: 0 USD</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/logout">Logout</Link>
-            </li>
+            <div className="nav__flex">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="/personalPage">MyPage</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/orders">Orders</Link>
+              </li>
+            </div>
+            <div className="nav__flex">
+              <li className="nav-item">
+                <Link className="nav-link" to="/logout">Logout</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/addCash">Pocket: 0 USD</Link>
+              </li>
+            </div>
           </>
         ) : (
           <>
--- client/src/components/OrdersList/OrdersList.jsx
+++ client/src/components/OrdersList/OrdersList.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import OneOrder from '../OneOrder/OneOrder';
+import './OrderList.scss';
 
 export default function OrdersList() {
   return (
-    <div>
-      <h1>Заказы</h1>
-      <Link to="/createorder" className="btn btn-primary">Создать заказ</Link>
-      <br />
-      <OneOrder />
+    <div className="order">
+      <div className="order__container">
+        <h1 className="order__title">Заказы</h1>
+        <Link to="/createorder" className="order__btn">Создать заказ</Link>
+      </div>
+      <div>
+        <div className="one-order">
+          <OneOrder />
+        </div>
+      </div>
     </div>
   );
 }
