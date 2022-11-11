4f11da15add3b1f6e4018933034fe8d9aa3e7122
Dmitr99 <grishko_dmitriy99@mail.ru>
2022-11-11 12:08:20 +0300

    FormVPN_functional

--- client/src/components/FormVPN/FormVPN.jsx
+++ client/src/components/FormVPN/FormVPN.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import './FormVPN.scss';
 
 export default function FormVPN() {
+  const dispatch = useDispatch();
   return (
     <div className="form-vpn">
       <div className="form-vpn__container">
         <h2 className="form-vpn__title">Add your VPN</h2>
-        <form action="" className="form-vpn__form">
+        <form
+          className="form-vpn__form"
+          onSubmit={(e) => {
+            e.preventDefault();
+            dispatch(addServersThunk(Object.fromEntries(new FormData(e.target))));
+          }}
+        >
           <div className="form-vpn__main-flex">
             <div className="form-vpn__inputs-flex">
               <input placeholder="ip" name="ip" className="form-vpn__input" type="text" />
