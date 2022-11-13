4ba145137838fce1a4a4c93ad953fb9e6e16e572
Simon Mishchenko <symon261731@gmail.com>
2022-11-13 15:41:13 +0300

    add credit card without animation

--- client/src/components/PocketForm/PocketForm.jsx
+++ client/src/components/PocketForm/PocketForm.jsx
@@ -1,28 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './PocketForm.scss';
+import Cards from 'react-credit-cards';
+import 'react-credit-cards/es/styles-compiled.css';
 
 export default function PocketForm() {
+  const [number, setNumber] = useState('');
+  const [name, setName] = useState('');
+  const [expiry, setExpiry] = useState('');
+  const [cvc, setCvc] = useState('');
+  const [focus, setFocus] = useState('');
   return (
+    // <div className="pocket-form">
+    //   <div className="pocket-form__box">
+    //     <form action="" className="pocket-form__main">
+    //       <div className="pocket-form__content">
+    //         <p className="pocket-form__text">payment amount</p>
+    //         <input placeholder="1-1000$" type="text" className="pocket-form__input" />
+    //         <p className="pocket-form__text">name on card</p>
+    //         <input placeholder="Jonh Uvanov" type="text" className="pocket-form__input" />
+    //         <p className="pocket-form__text">card number</p>
+    //         <input placeholder="Your card number" type="text" className="pocket-form__input" />
+    //         <div className="pocket-form__flex-text">
+    //           <p className="pocket-form__text">expire date</p>
+    //           <p className="pocket-form__text">security code</p>
+    //         </div>
+    //         <div className="pocket-form__flex">
+    //           <input placeholder="MM/YY" type="text" className="pocket-form__input input_date" />
+    //           <input placeholder="123" type="number" className="pocket-form__input input_code" />
+    //         </div>
+    //       </div>
+    //       <button type="submit" className="pocket-form__btn">Pay</button>
+    //     </form>
+    //   </div>
+    // </div>
     <div className="pocket-form">
       <div className="pocket-form__box">
-        <form action="" className="pocket-form__main">
-          <div className="pocket-form__content">
-            <p className="pocket-form__text">payment amount</p>
-            <input placeholder="1-1000$" type="text" className="pocket-form__input" />
-            <p className="pocket-form__text">name on card</p>
-            <input placeholder="Jonh Uvanov" type="text" className="pocket-form__input" />
-            <p className="pocket-form__text">card number</p>
-            <input placeholder="Your card number" type="text" className="pocket-form__input" />
-            <div className="pocket-form__flex-text">
-              <p className="pocket-form__text">expire date</p>
-              <p className="pocket-form__text">security code</p>
-            </div>
-            <div className="pocket-form__flex">
-              <input placeholder="MM/YY" type="text" className="pocket-form__input input_date" />
-              <input placeholder="123" type="number" className="pocket-form__input input_code" />
-            </div>
-          </div>
-          <button type="submit" className="pocket-form__btn">Pay</button>
+        <Cards number={number} name={name} expiry={expiry} cvc={cvc} focused={focus} />
+        <form className="pocket-form__form" action="">
+          <input
+            type="tel"
+            name="number"
+            placeholder="Card Number"
+            value={number}
+            onChange={(e) => setNumber(e.target.value)}
+            onFocus={(e) => setFocus(e.target.name)}
+            className="pocket-form__input"
+          />
+          <input
+            type="text"
+            name="name"
+            placeholder="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            onFocus={(e) => setFocus(e.target.name)}
+            className="pocket-form__input"
+          />
+          <input
+            type="tel"
+            name="expiry"
+            placeholder=" MM/YY expiry"
+            value={expiry}
+            onChange={(e) => setExpiry(e.target.value)}
+            onFocus={(e) => setFocus(e.target.name)}
+            className="pocket-form__input"
+          />
+          <input
+            type="tel"
+            name="number"
+            placeholder="CVC"
+            value={cvc}
+            onChange={(e) => setCvc(e.target.value)}
+            onFocus={(e) => setFocus(e.target.name)}
+            className="pocket-form__input"
+          />
+          <button className="pocket-form__btn" type="submit">Update</button>
         </form>
       </div>
     </div>
--- server/routes/pocketRouter.js
+++ server/routes/pocketRouter.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 const express = require('express');
 const { User, Card, Transaction } = require('../db/models');
 
@@ -6,10 +7,12 @@ const router = express.Router();
 // /api/pocket/refill - положить деньги на кошелек
 router.post('/refill', async (req, res) => {
   try {
-    const { amount, name, card_number, expire_date } = req.body;
+    const {
+      amount, name, card_number, expire_date,
+    } = req.body;
     const [card, created] = await Card.findOrCreate({
       where: { user_id: req.session.user.id, card_number },
-      defaults: { expire_date, name }
+      defaults: { expire_date, name },
     });
     await Transaction.create({ amount, user_id: req.session.user.id, card_id: card.id });
     const user = await User.findByPk(req.session.user.id);
@@ -19,8 +22,8 @@ router.post('/refill', async (req, res) => {
     return res.json(updateUser);
   } catch (error) {
     console.log(error);
-    return res.status(500).json({message: 'You broke my perfect database. Again.'})
+    return res.status(500).json({ message: 'You broke my perfect database. Again.' });
   }
-})
+});
 
 module.exports = router;
