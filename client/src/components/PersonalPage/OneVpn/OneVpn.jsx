08a74bfa7f9da4691b95331150d93338efae84c5
Nikita Boriskin <8858794@gmail.com>
2022-11-14 14:25:42 +0300

    downloadFileDone

--- client/src/components/PersonalPage/OneVpn/OneVpn.jsx
+++ client/src/components/PersonalPage/OneVpn/OneVpn.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 import './OneVpn.scss';
 
 export default function OneVpn({ info }) {
+  const [configLink, setConfigLink] = useState({});
+
+  function handleDownload() {
+    axios('server/config/:id', { responseType: 'blob' })
+      .then((res) => {
+        const href = URL.createObjectURL(res.data);
+        const download = 'config.ovpn';
+        setConfigLink({ href, download });
+        console.log({ res });
+      });
+  }
   return (
     <div className="one-vpn">
       <div className="one-vpn__box">
@@ -23,6 +35,10 @@ export default function OneVpn({ info }) {
           5
         </p>
       </div>
+      <button onClick={handleDownload} type="button" className="personal-page__btn" to="/">Download config</button>
+      {configLink?.href ? (
+        <a href={`${configLink?.href}`} download="config.ovpn"> config</a>
+      ) : (null)}
     </div>
   );
 }
--- client/src/components/PersonalPage/Order/Order.jsx
+++ client/src/components/PersonalPage/Order/Order.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Order.scss';
 
 export default function Order({ info }) {
-  console.log(info);
+  // console.log(info);
   return (
     <div className="current-order__box">
       <p className="current-order__title">{info?.title}</p>
