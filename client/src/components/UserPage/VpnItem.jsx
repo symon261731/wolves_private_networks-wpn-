7c6b44571f8f9bd0f79b2f1789cc7adb7ef47e17
Simon Mishchenko <symon261731@gmail.com>
2022-11-14 10:23:35 +0300

    fix styles in userPage

--- client/src/components/UserPage/VpnItem.jsx
+++ client/src/components/UserPage/VpnItem.jsx
@@ -3,13 +3,11 @@ import React from 'react';
 export default function VpnItem({ vpn }) {
   return (
     <li className="user-page__vpn-item">
-      <div className="user-page__vpn-info">
-        <p className="user-page__vpn-country">{vpn?.location}</p>
-        <p className="user-page__vpn-score">
-          {vpn?.rating}
-          ★
-        </p>
-      </div>
+      <p className="user-page__vpn-country">{vpn?.location}</p>
+      <p className="user-page__vpn-score">
+        {vpn?.rating}
+        ★
+      </p>
     </li>
   );
 }
