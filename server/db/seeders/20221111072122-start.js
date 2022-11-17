64bfd5c9cd97727427b1dcba6e84ef59d6cd23fb
psatin <ksyuha.93@mail.ru>
2022-11-17 13:24:57 +0300

    fix seeds

--- server/db/seeders/20221111072122-start.js
+++ server/db/seeders/20221111072122-start.js
@@ -44,49 +44,65 @@ module.exports = {
     await queryInterface.bulkInsert('Users', users, {});
     // ServerVPN
     const vpns = [];
-    for (let i = 0; i < 5; i += 1) {
+    const protocols =['WireGuard', 'OpenVPN', 'L2TP/IPsec']
+    for (let i = 0; i < 30; i += 1) {
       vpns.push({
-        //         ip: DataTypes.STRING,
-        // location: DataTypes.STRING,
-        // protocol: DataTypes.STRING,
-        // rating: DataTypes.INTEGER,
-        // user_id: DataTypes.INTEGER,
-        // price: DataTypes.INTEGER
         ip: faker.internet.ipv4(),
         location: faker.address.country(),
-        protocol: 'WireGuard',
-        rating: Math.floor(Math.random() * 1000),
-        user_id: Math.floor(Math.random() * 19 + 1),
-        price: Math.floor(Math.random() * 1000000),
+        protocol: protocols[Math.floor(Math.random() * 3)],
+        rating: Math.floor(Math.random() * 10000),
+        user_id: Math.floor(Math.random() * 20 + 1),
+        price: Math.floor(Math.random() * 10000),
         createdAt: new Date(),
         updatedAt: new Date(),
       });
     }
     await queryInterface.bulkInsert('ServerVPNs', vpns, {});
     // File
-    const files = [];
-    for (let i = 0; i < 40; i += 1) {
-      files.push({
-        //     user_id: DataTypes.INTEGER,
-        // config_path: DataTypes.STRING,
-        // server_id: DataTypes.INTEGER
-        user_id: Math.floor(Math.random() * 19 + 1),
-        server_id: Math.floor(Math.random() * 5 + 1),
-        config_path: '/very/secure/path',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      });
-    }
-    await queryInterface.bulkInsert('Files', files, {});
+    // const files = [];
+    // for (let i = 0; i < 40; i += 1) {
+    //   files.push({
+    //     //     user_id: DataTypes.INTEGER,
+    //     // config_path: DataTypes.STRING,
+    //     // server_id: DataTypes.INTEGER
+    //     user_id: Math.floor(Math.random() * 19 + 1),
+    //     server_id: Math.floor(Math.random() * 5 + 1),
+    //     config_path: '/very/secure/path',
+    //     createdAt: new Date(),
+    //     updatedAt: new Date(),
+    //   });
+    // }
+    //await queryInterface.bulkInsert('Files', files, {});
 
     const comments = [];
-    for (let i = 0; i < 100; i += 1) {
+    const text = [
+      // блок про сервера
+      'Found out today what this VPN had security breach and have poor PKI management. Attacker got access to one of the servers. That\'s enough to say goodbye. They can put millions in ads, but don\'t put money in security.',
+      'Good service! Recommend for all!',
+      'Great VPN service for reasonable price',
+      'Overpriced, IMHO',
+      'NC, the worst VPN in my life',
+      'First comment ever!',
+      'This server is the best answer your prayer. Anything else is a compromise. It\'s a bit pricier than other options. But it\'s solid AF. And it never take your information. Heck you could even pay by cash in an envelope with no return address. 100% most trustworthy.',
+      'VPN is very cheap, and does the job well for me',
+      'Works great for me',
+      'the worst purchase in my life!!!!! Don\'t use it!!! Seriously!!!!',
+      // блок про юзера
+      'Great guy! Khow him in person!',
+      'Made an order for me: fast, cheap and VPN is really working! Highly recommend!',
+      'I made an order for him and he didn\'t approve it! Where is my money, Lebovski?!!!!',
+      'Provide great service',
+      'You think you block me everywhere, asshole?! I find you! Don\'t trust this guy - take an order and do nothing in return!',
+      'Nice guy',
+      'The best VPN owner ever! His mom.'
+    ]
+    for (let i = 0; i < 17; i += 1) {
     //   user_id: DataTypes.INTEGER,
     // content: DataTypes.TEXT
       comments.push({
-        user_id: Math.floor(Math.random() * 19 + 1),
-        content: faker.commerce.productDescription(),
-        rating: 0,
+        user_id: Math.floor(Math.random() * 20 + 1),
+        content: text[i],
+        rating: Math.floor(Math.random() * 10000),
         createdAt: new Date(),
         updatedAt: new Date(),
       });
@@ -94,30 +110,34 @@ module.exports = {
     await queryInterface.bulkInsert('Comments', comments, {});
 
     const orders = [];
-    for (let i = 0; i < 20; i += 1) {
-    //   user_id: DataTypes.INTEGER,
-    // title: DataTypes.STRING,
-    // price: DataTypes.INTEGER,
-    // location: DataTypes.STRING,
-    // protocol: DataTypes.STRING
+    const titles = [
+      'need fast',
+      'URGENT!!!!',
+      'ready for price negotiation',
+      'pls',
+      'help me!',
+    ]
+    for (let i = 0; i < 5; i += 1) {
+      const country = faker.address.country();
       orders.push({
-        user_id: Math.floor(Math.random() * 19 + 1),
-        title: faker.commerce.productName(),
+        user_id: Math.floor(Math.random() * 20 + 1),
+        title: `${country.toUpperCase()} ${titles[Math.floor(Math.random() * 4)]}`,
         price: Math.floor(Math.random() * 1000000),
-        location: faker.address.country(),
-        protocol: 'very secure swear to God',
+        location: country,
+        protocol: protocols[Math.floor(Math.random() * 3)],
         status: 'in progress',
         createdAt: new Date(),
         updatedAt: new Date(),
       });
     }
-    for (let i = 0; i < 30; i += 1) {
+    for (let i = 0; i < 10; i += 1) {
+      const country = faker.address.country();
       orders.push({
-        user_id: Math.floor(Math.random() * 19 + 1),
-        title: faker.commerce.productName(),
+        user_id: Math.floor(Math.random() * 20 + 1),
+        title: `${country.toUpperCase()} ${titles[Math.floor(Math.random() * 4)]}`,
         price: Math.floor(Math.random() * 1000000),
-        location: faker.address.country(),
-        protocol: 'very secure swear to God',
+        location: country,
+        protocol: protocols[Math.floor(Math.random() * 3)],
         status: 'open',
         createdAt: new Date(),
         updatedAt: new Date(),
@@ -128,11 +148,9 @@ module.exports = {
     // ServerComment
     const temp = [];
     for (let i = 0; i < 50; i += 1) {
-    //   comment_id: DataTypes.INTEGER,
-    // server_id: DataTypes.INTEGER
       temp.push({
-        server_id: Math.floor(Math.random() * 5 + 1),
-        comment_id: Math.floor(Math.random() * 99 + 1),
+        server_id: Math.floor(Math.random() * 30 + 1),
+        comment_id: Math.floor(Math.random() * 10 + 1),
         createdAt: new Date(),
         updatedAt: new Date(),
       });
@@ -145,8 +163,8 @@ module.exports = {
     //   comment_id: DataTypes.INTEGER,
     // user_id: DataTypes.INTEGER
       temp2.push({
-        comment_id: Math.floor(Math.random() * 99 + 1),
-        user_id: Math.floor(Math.random() * 19 + 1),
+        comment_id: Math.floor(Math.random() * 7 + 10),
+        user_id: Math.floor(Math.random() * 20 + 1),
         createdAt: new Date(),
         updatedAt: new Date(),
       });
@@ -155,7 +173,7 @@ module.exports = {
 
     // OrderUser
     const temp3 = [];
-    for (let i = 0; i < 20; i += 1) {
+    for (let i = 0; i < 5; i += 1) {
     //   order_id: DataTypes.INTEGER,
     // creator: DataTypes.INTEGER,
     // worker: DataTypes.INTEGER
@@ -174,8 +192,8 @@ module.exports = {
     const purchases = [];
     for (let i = 0; i < 30; i += 1) {
       purchases.push({
-        user_id: Math.floor(Math.random() * 19 + 1),
-        server_id: Math.floor(Math.random() * 5 + 1),
+        user_id: Math.floor(Math.random() * 20 + 1),
+        server_id: Math.floor(Math.random() * 30 + 1),
         createdAt: new Date(),
         updatedAt: new Date(),
       });
